428224864ffcb190372bd5ae72d82c75d74d2bf2
Yiyang (Jessie) Yu <yyu69@uclive.ac.nz>
2022-07-26 14:52:31 +1200

    q11 doesnt work

--- 1-Cost-insensitive-search/q11-SlidingPuzzleGraph.py
+++ 1-Cost-insensitive-search/q11-SlidingPuzzleGraph.py
@@ -1,58 +1,117 @@
-from search import  *
+from search import *
+import copy
+import collections  # importing "collections" for deque operations
+
+BLANK = ' '
+
 
 class SlidingPuzzleGraph(Graph):
-    """implements (n squared minus one)-puzzles."""
+    """Objects of this type represent (n squared minus one)-puzzles.
+    """
 
-    def __init__(self, nodes, edge_list, starting_nodes, goal_nodes, estimates=None):
-        """Initialises an explicit graph.
-        Keyword arguments:
-        nodes -- a set of nodes
-        edge_list -- a sequence of tuples in the form (tail, head) or
-                     (tail, head, cost)
-        starting_nodes -- the list of starting nodes. We use a list
-                          to remind you that the order can influence
-                          the search behaviour.
-        goal_node -- the set of goal nodes. It's better if you use a set
-                     here to remind yourself that the order does not matter
-                     here. This is used only by the is_goal method.
-        """
+    def __init__(self, starting_state):
+        self.starting_state = starting_state
 
-        # A few assertions to detect possible errors in
-        # instantiation. These assertions are not essential to the
-        # class functionality.
-        assert all(tail in nodes and head in nodes for tail, head, *_ in edge_list) \
-            , "An edge must link two existing nodes!"
-        assert all(node in nodes for node in starting_nodes), \
-            "The starting_states must be in nodes."
-        assert all(node in nodes for node in goal_nodes), \
-            "The goal states must be in nodes."
+    def outgoing_arcs(self, state):
+        """Given a puzzle state (node) returns a list of arcs. Each arc
+        represents a possible action (move) and the resulting state."""
 
-        self.nodes = nodes
-        self.edge_list = edge_list
-        self._starting_nodes = starting_nodes
-        self.goal_nodes = goal_nodes
-        self.estimates = estimates
+        n = len(state)  # the size of the puzzle
+
+        # Find i and j such that state[i][j] == BLANK
+        for row in range(n):
+            for col in range(n):
+                print(state[row][col])
+                if state[row][col] == BLANK:
+                    i, j = row, col
+                    break
+        # [for row in range (n) for col in range (n) if state[row][col] == BLANK]
+
+        arcs = []
+        if i > 0:
+            action = "Move {} down".format(state[i - 1][j])  # or blank goes up
+            new_state = copy.deepcopy(state)
+            # new_state[i][j] is where blank is
+            # new_state[i][j] / the blank get swapped with new_state[i - 1][j]
+            new_state[i][j], new_state[i - 1][j] = new_state[i - 1][j], BLANK
+            arcs.append(Arc(state, new_state, action, 1))
+        if i < n - 1:
+            action = "Move {} up".format(state[i + 1][j])  # or blank goes down
+            new_state = copy.deepcopy(state)
+            new_state[i][j], new_state[i + 1][j] = new_state[i + 1][j], BLANK
+            arcs.append(Arc(state, new_state, action, 1))
+        if j > 0:
+            action = "Move {} right".format(state[i][j - 1])  # or blank goes left
+            new_state = copy.deepcopy(state)
+            new_state[i][j], new_state[i][j - 1] = new_state[i][j - 1], BLANK
+            arcs.append(Arc(state, new_state, action, 1))
+        if j < n - 1:
+            action = "Move {} left".format(state[i][j + 1])  # or blank goes right
+            new_state = copy.deepcopy(state)
+            new_state[i][j], new_state[i][j + 1] = new_state[i][j + 1], BLANK
+            arcs.append(Arc(state, new_state, action, 1))
+        return arcs
 
     def starting_nodes(self):
-        """Returns a sequence of starting nodes."""
-        return self._starting_nodes
+        return [self.starting_state]
 
-    def is_goal(self, node):
-        """Returns true if the given node is a goal node."""
-        return node in self.goal_nodes
+    def is_goal(self, state):
+        """Returns true if the given state is the goal state, False
+        otherwise. There is only one goal state in this problem."""
 
-    def outgoing_arcs(self, node):
-        """Returns a sequence of Arc objects that go out from the given
-        node. The action string is automatically generated.
+        n = len(state)
+        goal = [num for num in range(n * n)]
 
-        """
-        arcs = []
-        for edge in self.edge_list:
-            if len(edge) == 2:  # if no cost is specified
-                tail, head = edge
-                cost = 1  # assume unit cost
-            else:
-                tail, head, cost = edge
-            if tail == node:
-                arcs.append(Arc(tail, head, str(tail) + '->' + str(head), cost))
-        return arcs
+        # converting 2d list into 1d
+        # using list comprehension
+        state_1d = [element for row in state for element in row]
+
+        for i in range(n * n):
+            if goal[i] != state_1d[i]:
+                return False
+
+        return True
+
+
+class BFSFrontier(Frontier):
+    """Implements a frontier container appropriate for breadth-first
+        search."""
+
+    def __init__(self, ):
+        """The constructor takes no argument. It initialises the
+        container to an empty stack."""
+        self.container = collections.deque([])
+
+    def add(self, path):
+        """Store the given path by adding a new path to the frontier. A path is a sequence (tuple) of
+        Arc objects. You should override this method. """
+        self.container.append(path)
+
+    def __iter__(self):
+        """We don't need a separate iterator object. Just return self. You
+        don't need to change this method."""
+        return self
+
+    def __next__(self):
+        """Selects, removes, and returns a path on the frontier if there is
+        any. Recall that a path is a sequence (tuple) of Arc
+        objects. Override this method to achieve a desired search
+        strategy. If there nothing to return this should raise a
+        StopIteration exception."""
+        if len(self.container) > 0:
+            return self.container.popleft()
+        else:
+            raise StopIteration
+
+
+def main():
+    graph = SlidingPuzzleGraph([[1, 2, 5],
+                                [3, 4, 8],
+                                [6, 7, ' ']])
+
+    solutions = generic_search(graph, BFSFrontier())
+    print_actions(next(solutions))
+
+
+if __name__ == "__main__":
+    main()
